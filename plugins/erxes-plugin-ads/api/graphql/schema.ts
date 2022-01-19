c411473c535a662d6af7c5d4a80ce934cc6a70d7
soyombo <soyombo.b@nmtec.co>
2022-01-19 17:47:08 +0800

    add submision detail to plugin

--- plugins/erxes-plugin-ads/api/graphql/schema.ts
+++ plugins/erxes-plugin-ads/api/graphql/schema.ts
@@ -3,26 +3,8 @@ export const types = `
     customerId: String
     loyalty: Float
   }
-
-  type Loyalty {
-    modifiedAt: Date,
-    customerId: String,
-    value: Float,
-    dealId: String,
-    userId: String,
-
-    user: User
-    customer: Customer
-    deal: Deal
-  }
-
-
 `;
 
 export const queries = `
-  ads(
-    tagId: String
-    page: Int
-    perPage: Int
-  ): JSON
+  formSubmissionDetail(contentTypeId: String!): Submission
 `;
