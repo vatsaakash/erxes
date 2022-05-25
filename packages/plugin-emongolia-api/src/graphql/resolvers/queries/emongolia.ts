828afa58bb01995d7e43464585b8bfd9dd7f43f3
Ariukasq1 <Ariuka.sq@gmail.com>
2022-05-26 06:52:34 +0800

    emongolia update

--- packages/plugin-emongolia-api/src/graphql/resolvers/queries/emongolia.ts
+++ packages/plugin-emongolia-api/src/graphql/resolvers/queries/emongolia.ts
@@ -1,6 +1,22 @@
+var base64 = require('base-64');
+const redirectUrl = 'https://www.rentpay.mn/emongolia/receive';
+const clientId = '8bd4872e35ee93e32c33097c-af7c6f5ec773c96a37e2b4ce4f7a4049';
+
 const danLoginQueries = {
-  getDanLogin: async (_root, { customerId }, { models }) => {
-    return await models.getDanLogin.find({ customerId });
+  getLoginUrl: (_root, { serviceName, value }) => {
+    const name = serviceName;
+    const service_structure = [
+      {
+        services: name,
+        wsdl: 'https://xyp.gov.mn/citizen-1.3.0/ws?WSDL' // wsdl зам
+        // Оролтын параметргүй дуудагддаг сервис
+      }
+    ];
+    const scope = base64.encode(JSON.stringify(service_structure));
+
+    const url = `https://sso.gov.mn/oauth2/authorize?response_type=code&client_id=${clientId}&redirect_uri=${redirectUrl}&scope=${scope}state=afc7e00ab9250f59c2849bae67d90d1eb54459d8eb712c4c306bee5f287f2cb1`;
+
+    return url;
   }
 };
 
