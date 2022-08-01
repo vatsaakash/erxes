4973e7250c22ed1b81215e31aef4f53e68982ee5
Baterdene Khashbat <hashbaterdene@gmail.com>
2022-08-01 18:22:52 +0800

    change default port

--- packages/plugin-posclient-ui/src/configs.js
+++ packages/plugin-posclient-ui/src/configs.js
@@ -1,22 +1,23 @@
 module.exports = {
   name: 'posclient',
-  port: 3012,
+  port: 3099,
   scope: 'posclient',
   exposes: {
-    './routes': './src/routes.tsx'
+    './routes': './src/routes.tsx',
   },
   routes: {
-    url: 'http://localhost:3012/remoteEntry.js',
+    url: 'http://localhost:3099/remoteEntry.js',
     scope: 'posclient',
-    module: './routes'
+    module: './routes',
   },
   menus: [
     {
       text: 'Posclients',
-      to: '/posclients',
+      to: 'http://localhost:3099/',
+      blank: true,
       image: '/images/icons/erxes-18.svg',
       location: 'settings',
-      scope: 'posclient'
-    }
-  ]
+      scope: 'posclient',
+    },
+  ],
 };
